42fa8b894f1f75cf575ca7f539bf058a0f8f6c9a
Joscelyn James <josce.james7@hotmail.com>
2017-09-08 15:07:49 -0600

    seeds ran

--- seeds/blog_post.js
+++ seeds/blog_post.js
@@ -2,8 +2,28 @@
 exports.seed = function(knex, Promise) {
   return knex('blog_post').del()
     .then(function () {
-      return knex('blo_post').insert([
-        {id: 1, colName: 'rowValue1'},
+      return knex('blog_post').insert([
+        {
+          title: "Wearing Socks with Sandals",
+          body: "First test",
+          author: "Mary Beth",
+          creation_time: new Date(),
+          // website_id: 1,
+        },
+        {
+          title: "Is there such a thing as too high of pants?",
+          body: "Second test",
+          author: "Mary Beth jr",
+          creation_time: new Date(),
+          // website_id: 1,
+        },
+        {
+          title: "Creepy Rape Glasses from the 80's",
+          body: "Third test",
+          author: "Mary Beth sr",
+          creation_time: new Date(),
+          // website_id: 1,
+        },
       ]);
     });
 };
--- seeds/comment.js
+++ seeds/comment.js
@@ -3,7 +3,48 @@ exports.seed = function(knex, Promise) {
   return knex('comment').del()
     .then(function () {
       return knex('comment').insert([
-        {id: 1, colName: 'rowValue1'},
+        {
+          body: 'Im Angry',
+          author_name: 'Ann Frank',
+          creation_time: new Date(),
+          // blog_id: 1,
+        },
+        {
+          body: 'I like turtles',
+          author_name: 'Ann Frank',
+          creation_time: new Date(),
+          // blog_id: 1,
+        },
+        {
+          body: 'Im Happy',
+          author_name: 'Susan Susan',
+          creation_time: new Date(),
+          // blog_id: 2,
+        },
+        {
+          body: 'America',
+          author_name: 'Ron Swanson',
+          creation_time: new Date(),
+          // blog_id: 2,
+        },
+        {
+          body: 'Im vegan',
+          author_name: 'Derp Derp',
+          creation_time: new Date(),
+          // blog_id: 3,
+        },
+        {
+          body: 'Lifes ok right now...',
+          author_name: 'Craig Hammilton',
+          creation_time: new Date(),
+          // blog_id: 3,
+        },
+        {
+          body: 'Pooptarts',
+          author_name: 'Marge Burns',
+          creation_time: new Date(),
+          // blog_id: 3,
+        },
       ]);
     });
 };
