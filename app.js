9dbf9f47fce50fe110789145589ae7099168d1ba
Joscelyn James <josce.james7@hotmail.com>
2017-09-13 14:04:08 -0600

    added module exprots to app.js

--- app.js
+++ app.js
@@ -12,12 +12,24 @@ app.use(bodyParser.json())
 
 app.use(routes)
 
-app.use(function (req, res, next) {
-  const err = new Error('not found')
-  err.status = 404
-  next(err)
-})
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: req.app.get('env') === 'development' ? err : {}
+  });
+});
 
 app.listen(port, () => {
   console.log('Listening on port ' + port)
 })
+
+module.exports = app;
