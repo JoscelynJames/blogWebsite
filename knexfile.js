24c9dd0d5e0c00db25c6afc550c6f51bba0f7df1
Joscelyn James <josce.james7@hotmail.com>
2017-09-13 10:30:06 -0600

    deploying to heroku

--- knexfile.js
+++ knexfile.js
@@ -4,11 +4,11 @@ module.exports = {
 
   development: {
     client: 'pg',
-    connection: 'postgres://localhost/gblogdb',
-},
+    connection: 'postgres://localhost/gblogdb'
+  },
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URI,
+    connection: 'postgres://bsopcikjhqwaiv:4810a39ba0c85c97b34464ec2387d8a5f54e3e4f640835e2b751e1aa0effb022@ec2-54-225-192-243.compute-1.amazonaws.com:5432/d4hh0h7j3etb53'
   }
 
-};
+}
