77b72c3445d24bcd4b30161fe6479edade06e43b
Joscelyn James <josce.james7@hotmail.com>
2017-09-13 10:57:27 -0600

    dsaff

--- knexfile.js
+++ knexfile.js
@@ -8,10 +8,7 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: 'postgres://bsopcikjhqwaiv:4810a39ba0c85c97b34464ec2387d8a5f54e3e4f640835e2b751e1aa0effb022@ec2-54-225-192-243.compute-1.amazonaws.com:5432/d4hh0h7j3etb53'
-  },
-  migrations: {
-    tableName: 'knex_migrations'
+    connection: process.env.DATABASE_URL
   }
 
 }
